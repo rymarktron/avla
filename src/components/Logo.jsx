69107da307c07ad8bb461554e158e8442ca98d11
rymarktron <87295558+rymarktron@users.noreply.github.com>
2023-03-03 21:21:29 -0500

    First commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,37 @@
+export function Logo(props) {
+  return (
+    <svg
+    data-name="Layer 2"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 167.34 42"
+    {...props}
+  >
+    <g data-name="Layer 1">
+      <path
+        d="M33.18 32.28h-21L8.04 42H0L18.9 0h7.68l18.96 42h-8.16l-4.2-9.72Zm-2.58-6.12L22.68 7.8l-7.86 18.36H30.6Z"
+        style={{
+          fill: "#2a3990",
+        }}
+      />
+      <path
+        d="m87.06 0-18.3 42h-7.68L42.72 0h8.46l13.98 32.4L79.26 0h7.8Z"
+        style={{
+          fill: "#ca8ebf",
+        }}
+      />
+      <path
+        d="M91.5 0h7.8v35.4h21.96V42H91.5V0Z"
+        style={{
+          fill: "#5e9ed5",
+        }}
+      />
+      <path
+        d="M154.98 32.28h-21L129.84 42h-8.04l18.9-42h7.68l18.96 42h-8.16l-4.2-9.72Zm-2.58-6.12L144.48 7.8l-7.86 18.36h15.78Z"
+        style={{
+          fill: "#f7a451",
+        }}
+      />
+    </g>
+  </svg>
+  )
+}
