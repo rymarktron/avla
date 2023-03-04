75b477d973ffb52d95c2f6c1dba7389311d0803c
rymarktron <87295558+rymarktron@users.noreply.github.com>
2023-03-04 15:09:04 -0500

    Azure API added

--- src/pages/demo.jsx
+++ src/pages/demo.jsx
@@ -6,7 +6,44 @@ import { Button } from '@/components/Button'
 import { SelectField, TextField } from '@/components/Fields'
 import { Logo } from '@/components/Logo'
 
+//For Azure Text to Speech API
+import { useState } from 'react';
+import * as sdk from 'microsoft-cognitiveservices-speech-sdk';
+
 export default function Demo() {
+  //Use States used
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [text, setText] = useState('');
+  const speechConfig = sdk.SpeechConfig.fromSubscription(process.env.NEXT_PUBLIC_TEXT_TO_SPEECH_API_KEY, 'eastus');
+  
+  const synthesizeSpeech = () => {
+    const audioConfig = sdk.AudioConfig.fromDefaultSpeakerOutput();
+    const synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+    const ssml = `<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
+                   <voice name='en-US-JessaNeural'>
+                     ${text}
+                   </voice>
+                 </speak>`;
+    const result = synthesizer.speakSsmlAsync(
+      ssml,
+      (event) => {
+        if (event.name === 'start') {
+          setIsSpeaking(true);
+        } else if (event.name === 'end') {
+          setIsSpeaking(false);
+        }
+      },
+      (error) => {
+        console.error(error);
+        setIsSpeaking(false);
+      }
+    );
+  };
+  
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+  
   return (
     <>
       <Head>
@@ -25,80 +62,35 @@ export default function Demo() {
             <h2 className="text-lg font-semibold text-gray-900">
               Welcome. Enter a prompt and we will generate some questions.
             </h2>
-            <p className="mt-2 text-sm text-gray-700">
-              Already registered?{' '}
-              <Link
-                href="/login"
-                className="font-medium text-orange-400 hover:underline"
-              >
-                Sign in
-              </Link>{' '}
-              to your account.
-            </p>
+
           </div>
         </div>
-        <form
-          action="#"
-          className="grid grid-cols-1 mt-10 gap-y-8 gap-x-6 sm:grid-cols-2"
-        >
-          <TextField
-            label="First name"
-            id="first_name"
-            name="first_name"
+        <form action="#" className="grid grid-cols-1 mt-10 gap-y-8">
+        <TextField
+            label="Prompt"
+            id="prompt"
+            name="prompt"
             type="text"
-            autoComplete="given-name"
+            autoComplete="prompt"
             required
+            value={text}
+            onChange={handleTextChange}
           />
-          <TextField
-            label="Last name"
-            id="last_name"
-            name="last_name"
-            type="text"
-            autoComplete="family-name"
-            required
-          />
-          <TextField
-            className="col-span-full"
-            label="Email address"
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-          />
-          <TextField
-            className="col-span-full"
-            label="Password"
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="new-password"
-            required
-          />
-          <SelectField
-            className="col-span-full"
-            label="How did you hear about us?"
-            id="referral_source"
-            name="referral_source"
-          >
-            <option>AltaVista search</option>
-            <option>Super Bowl commercial</option>
-            <option>Our route 34 city bus ad</option>
-            <option>The “Never Use This” podcast</option>
-          </SelectField>
-          <div className="col-span-full">
+        </form>
+
+            
+        <div className="col-span-full">
             <Button
               type="submit"
               variant="solid"
               color="orange"
               className="w-full"
+              onClick={synthesizeSpeech} 
+              disabled={isSpeaking}
             >
-              <span>
-                Sign up <span aria-hidden="true">&rarr;</span>
-              </span>
+              {isSpeaking ? 'Speaking...' : 'Speak'}
             </Button>
-          </div>
-        </form>
+        </div>
       </DemoLayout>
     </>
   )
